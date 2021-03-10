f53f888f5c8cbebad5f958a3aae71670b3afd0b8
Daniel DeGroff <daniel@fusionauth.io>
2021-03-09 22:27:06 -0700

    Fix tests

--- src/test/java/com/mycompany/fusionauth/plugins/ExamplePBDKF2HMACSHA1PasswordEncryptorTest.java
+++ src/test/java/com/mycompany/fusionauth/plugins/ExamplePBDKF2HMACSHA1PasswordEncryptorTest.java
@@ -32,8 +32,8 @@ public class ExamplePBDKF2HMACSHA1PasswordEncryptorTest {
   @DataProvider(name = "hashes")
   public Object[][] hashes() {
     return new Object[][]{
-        {"password123", "1484161696d0ca62390273b98846f49671cecd78", "4761d3392092f9ca6036b53dc92c6d7f3d597576"},
-        {"password123", "ea95629c7954d73ea670f07a798e9fd4ab907593", "9480ad9a59cb5053b832ba5e731afcd1f78068ec"},
+        {"password123", "1484161696d0ca62390273b98846f49671cecd78", "4761D3392092F9CA6036B53DC92C6D7F3D597576"},
+        {"password123", "ea95629c7954d73ea670f07a798e9fd4ab907593", "9480AD9A59CB5053B832BA5E731AFCD1F78068EC"},
     };
   }
 }
